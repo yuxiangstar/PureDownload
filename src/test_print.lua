89a8a9b555f7f49643d475a496dc9fb8ebe251f8
yuxiang_sun <yuxiang_sun@intsig.net>
2024-09-23 18:39:35 +0800

    测试路由3

--- src/test_print.lua
+++ src/test_print.lua
@@ -1,5 +1,5 @@
 --local cjson = require("cjson")
 local args = ngx.req.get_uri_args()
-local encode = require("cjson.safe").encode
+--local encode = require("cjson.safe").encode
 ngx.print("hello lualib"..encode(args))
 ngx.exit(200)
